9c174226434696f1116f76b1e2375d9009e226a2
J.J. Tolton <jjtolton@gmail.com>
2017-12-20 16:37:22 -0500

    added "and" and "or" macros, added stupid hack to `let` to work for corner cases

--- lib/macros.py
+++ lib/macros.py
@@ -1,7 +1,7 @@
 from naga import partition
 
 from lib.destructure import destructure
-from lib.symbols import def_, fn_, quote_
+from lib.symbols import def_, fn_, quote_, if_, Symbol, let_
 from lib.utils import isa
 
 
@@ -22,7 +22,10 @@ def defn(*args):
 
 
 def let(forms, exps):
-    forms = destructure(forms)
+    try:
+        forms = destructure(forms)
+    except IndexError:
+        forms = destructure([a for b in forms for a in b])
 
     def _let(forms, exps):
         for a, b in forms:
@@ -32,22 +35,25 @@ def let(forms, exps):
     nforms = iter(list(partition(2, destructure(forms)))[::-1])
     return _let(nforms, exps)
 
+def and_(*args):
+    if len(args) == 1:
+        return args[0]
+    return [if_, args[0],
+            [Symbol('and'), *args[1:]],
+            args[0]]
 
 
-
-
+def or_(*args):
+    if len(args) == 1:
+        return args[0]
+    return [if_, args[0],
+            args[0],
+            [Symbol('or'), *args[1:]]]
 
 
 macro_table = {'defn': defn,
+               'and': and_,
+               'or': or_,
                'quote': quote,
                'let': let}  ## More macros can go here
 
-
-def tests():
-    from bracket import parse
-
-    print(str(parse('[let [[a b] /[1 2]] a]')).replace("'", '').replace(',', ''))
-
-
-if __name__ == '__main__':
-    tests()
